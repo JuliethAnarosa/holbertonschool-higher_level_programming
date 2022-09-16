e2ce5d9d7a4e93aef76bd0353b7b29960241833b
JuliethAnarosa <america02anarosa@gmail.com>
2022-09-15 19:32:02 -0500

    data type fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i in my_string:
-        if i == 'c' or i == 'C':
+    for char in my_string:
+        if char == 'c' or char == 'C':
             continue
         else:
-            new_string += i
-            return new_string
+            new_string += char
+    return new_string
