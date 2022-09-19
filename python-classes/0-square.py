48165c0196831dca1c2f4a8a744d74e7411b1636
JuliethAnarosa <america02anarosa@gmail.com>
2022-09-19 00:58:11 -0500

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """module square"""
-  class Square:
-      """Write an empty class Square that defines a square."""
-      pass
+class Square:
+  """Write an empty class Square that defines a square."""
+    pass
