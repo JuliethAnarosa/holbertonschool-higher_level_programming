970ceef9c2ecedc4ff5d6ae8e0218480c8f02574
JuliethAnarosa <america02anarosa@gmail.com>
2022-09-25 21:43:13 -0500

    task3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-
-"""a class Square that defines a
-square by: (based on 1-square.py)"""
+""" Module Sqaure """
 
 
 class Square:
-    """Raise Error using conditionals"""
+    """ Square class defined by geometric shap
+        Attributes:
+            size (int): Size of square
+    """
     def __init__(self, size=0):
-        if type(size) is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        else:
+        """
+        initializes the square
+        Args:
+            size (int): size of a side of the square
+        Raises:
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        return self.__size * self.
+        """
+            set square square area
+            Return:
+                the current square area (int)
+        """
+        return self.__size ** 2
