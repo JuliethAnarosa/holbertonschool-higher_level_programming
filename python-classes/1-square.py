213da3305f09e1707bcba5d9aa5763689f95c2f7
JuliethAnarosa <america02anarosa@gmail.com>
2022-09-25 21:22:33 -0500

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""empty class that defines a square"""
+
+
+class Square:
+    """Private instance attribtute size
+    Instantiation with size"""
+    def __init__(self, size):
+        self.__size = size
