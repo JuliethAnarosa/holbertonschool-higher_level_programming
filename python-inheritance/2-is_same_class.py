b95b9cbb3c6226ff2a4c45e58d5f7950a338455a
JuliethAnarosa <america02anarosa@gmail.com>
2022-10-02 19:31:12 -0500

    type of class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        False
