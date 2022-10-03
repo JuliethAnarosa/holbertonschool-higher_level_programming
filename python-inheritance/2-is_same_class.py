4293af666d4a3beee97e9ee2965ac25343932de1
JuliethAnarosa <america02anarosa@gmail.com>
2022-10-02 19:35:35 -0500

    operators fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     """
     returns True if the object is exactly an instance
     """
-    if type(obj) is a_class:
+    if type(obj) == a_class:
         return True
     else:
         False
