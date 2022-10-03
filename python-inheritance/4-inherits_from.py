52ce48c284eb9991f8c8c69a407e0e7fc9d2726c
JuliethAnarosa <america02anarosa@gmail.com>
2022-10-02 21:08:08 -0500

    issubclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,9 @@ instance of a class that inherited
 
 
 def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    """
     if issubclass(type(obj), a_class):
         return True
     else:
