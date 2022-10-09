2e07526a23e844a166e02b63cbe7943495592930
JuliethAnarosa <america02anarosa@gmail.com>
2022-10-09 14:52:20 -0500

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-the class Rectangle that inherits from Base
+Contains the "Rectangle" class
 """
-from .base import Base
+
+from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    A representation of a rectangle
-    """
+    """A representation of a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Init Class
-        """
+        """Initializes the rectangle"""
         self.width = width
         self.height = height
         self.x = x
@@ -21,42 +18,27 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-            Width Getter
-        """
+        """getter od width"""
         return self.__width
 
     @property
     def height(self):
-        """
-            height Getter
-        """
+        """getter of height"""
         return self.__height
 
     @property
     def x(self):
-        """
-            x Getter
-        """
+        """getter of x"""
         return self.__x
 
     @property
     def y(self):
-        """
-            y Getter
-        """
+        """getter of y"""
         return self.__y
 
     @width.setter
     def width(self, value):
-        """
-            Width Setter
-        Attribute:
-            Value(int): value to assign
-        Raises:
-            TypeError: Value must be int
-            ValueError: Value must be > 0
-        """
+        """setter of width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -65,30 +47,16 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """
-            height Setter
-        Attribute:
-            Value(int): value to assign
-        Raises:
-            TypeError: Value must be int
-            ValueError: Value must be > 0
-        """
+        """setter of height"""
         if type(value) is not int:
-                raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
-        """
-            x Setter
-        Attribute:
-            Value(int): value to assign
-        Raises:
-            TypeError: Value must be int
-            ValueError: Value must be >= 0
-        """
+        """setter of x"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -97,38 +65,25 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """
-            Width Setter
-        Attribute:
-            Value(int): value to assign
-        Raises:
-            TypeError: Value must be int
-            ValueError: Value must be >= 0
-        """
+        """setter of y"""
         if type(value) is not int:
-                raise TypeError("y must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
-        """
-            Define The Area of Rectangle
-        """
+        """calculates the area of the rectangle"""
         return self.__width * self.__height
 
     def display(self):
-        """
-            Display The Rectangle Using  '#'
-        """
+        """print a display of the rectangle"""
         print(("\n" * self.__y) +
               "\n".join(((" " * self.__x) + ("#" * self.__width))
                         for i in range(self.__height)))
 
     def __str__(self):
-        """
-            String Informal of the Rectangle
-        """
+        """informal string representation of the rectangle"""
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
                                                                  self.__x,
                                                                  self.__y,
@@ -136,31 +91,37 @@ class Rectangle(Base):
                                                                  self.__height)
 
     def update(self, *args, **kwargs):
-        """
-            Update Multiple Atrr of The Rectangle
-        """
-        i = 0
-        if args:
-            for arg in args:
+        """updates multiple attributes"""
+        if len(args):
+            for i, a in enumerate(args):
                 if i == 0:
-                    self.id = arg
-                if i == 1:
-                    self.width = arg
-                if i == 2:
-                    self.height = arg
-                if i == 3:
-                    self.x = arg
-                if i == 4:
-                    self.y = arg
-                i += 1
+                    self.id = a
+                elif i == 1:
+                    self.width = a
+                elif i == 2:
+                    self.height = a
+                elif i == 3:
+                    self.x = a
+                elif i == 4:
+                    self.y = a
         else:
-            for arg in kwargs:
-                setattr(self, arg, kwargs.get(arg))
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """
-            returns the dictionary
-            representation of a Rectangle
-        """
-        return {'id': self.id, 'width': self.width,
-                'height': self.height, 'x': self.x, 'y': self.y}
+        """dictionary representation of a Rectangle"""
+        d = {}
+        d["id"] = self.id
+        d["width"] = self.width
+        d["height"] = self.height
+        d["x"] = self.x
+        d["y"] = self.y
+        return d
