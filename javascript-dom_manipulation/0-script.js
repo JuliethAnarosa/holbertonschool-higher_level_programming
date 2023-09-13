a449988f9f50ed2e17a4c5b173e335bad72776c8
JuliethAnarosa <america02anarosa@gmail.com>
2023-09-13 10:12:58 -0500

    task

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar el elemento de cabecera por su etiqueta
+    var header = document.querySelector("header");
+  
+    // Actualizar el color del texto a rojo (#FF0000)
+    header.style.color = "#FF0000";
+  });
+  
